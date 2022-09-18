12b83694b9400c7994672fde2de6b0812f8a4361
oldholyfather <113419458+oldholyfather@users.noreply.github.com>
2022-09-18 09:47:07 +0300

    RandomGame

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello,dude!");
+
+Random n = new Random();
+int x = n.Next(50);
+
+Console.WriteLine("Напишите число от 0 до 50");
+
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == y)
+{
+    Console.WriteLine("Вы отгадали число");
+    Environment.Exit(0);
+}
+else
+{
+    Console.WriteLine("Вы не угадали число, попробуйте еще раз");
+}
+
+int r = Convert.ToInt32(Console.ReadLine());
+if (x == r)
+{
+    Console.WriteLine("Вы отгадали число");
+    Environment.Exit(0);
+}
+else
+{
+    Console.WriteLine("Вы не угадали число, попробуйте еще раз");
+}
+
+int g = Convert.ToInt32(Console.ReadLine());
+if (x == g)
+{
+    Console.WriteLine("Вы отгадали число");
+    Environment.Exit(0);
+}
+else
+{
+    Console.WriteLine("Вы не угадали число, попробуйте еще раз");
+}
